cb9c14ff0fe218a3bea45ad14ede936c209dfc75
Muiyaho <discordBotMaybe@gmail.com>
2023-04-05 15:06:59 +0900

    1.1 UI 수정 및 Parameters Update 기능 추가

--- queryBinder.py
+++ queryBinder.py
@@ -6,30 +6,22 @@ import tkinter.messagebox as messagebox
 
 def bind_query(query_template, params):
     query_template = re.sub(r'\?', r"'{}'", query_template)
-    try:
-        return query_template.format(*params)
-    except IndexError:
-        return "Error: Parameter count mismatch in the query template."
+    return query_template.format(*params)
 
 
 def extract_query_params(log_text):
     param_pattern = r'Parameters: \[(.*?)\]'
-    param_match = re.search(param_pattern, log_text, re.DOTALL)
+    param_match = re.search(param_pattern, log_text)
 
-    if param_match:
-        params_string = param_match.group(1)
-        params = [p.strip() for p in re.split(r',(?=(?:[^"]*"[^"]*")*[^"]*$)', params_string)]
-    else:
-        params = []
-
+    params = param_match.group(1).split(',') if param_match else []
     return params
 
 
 def extract_binding_vars(query_template):
     matches = re.findall(r"(\w+)\s*=\s*\?", query_template)
-    question_marks_count = query_template.count("?")
+    question_marks = query_template.count("?")
 
-    while len(matches) < question_marks_count:
+    while len(matches) < question_marks:
         matches.append("temp")
 
     return matches
@@ -41,8 +33,6 @@ def submit():
 
     params = extract_query_params(log_text)
     binding_vars = extract_binding_vars(query_template)
-
-    query_template = adjust_query_template(query_template, params)
 
     for i, (param, binding_var) in enumerate(zip(params, binding_vars)):
         param_name_labels[i].config(text=f"{binding_var}:")
@@ -57,8 +47,7 @@ def submit():
 
     update_button.grid(row=10, column=0, pady=(10, 0))
     clipboard_button.grid(row=10, column=1, pady=(10, 0))
-    update_param_entries()
-
+    update_params()
 
 def clear():
     query_text.delete("1.0", tk.END)
@@ -72,36 +61,29 @@ def clear():
 def copy_to_clipboard():
     root.clipboard_clear()
     root.clipboard_append(result_text.get("1.0", tk.END).strip())
-    messagebox.showinfo("복사 완료", "복사 되었습니다.")
+    messagebox.showinfo("복사 완료", "클립보드로 복사 되었습니다.")
 
 
-def adjust_query_template(query_template, params):
-    param_count = query_template.count("?")
-    if param_count < len(params):
-        query_template += ", ?" * (len(params) - param_count)
-    elif param_count > len(params):
-        for _ in range(param_count - len(params)):
-            query_template = query_template.rsplit("?", 1)[0]
-    return query_template
-
-
-def update_param_entries():
+def update_params():
     log_text = query_text.get("1.0", tk.END).strip()
     query_template = log_text.split("DEBUG")[0].strip()
     binding_vars = extract_binding_vars(log_text)
 
-    params = extract_query_params(log_text)
-
+    params = []
     for i, binding_var in enumerate(binding_vars):
         param_name_labels[i].config(text=f"{binding_var}:")
-        param_entries[i].delete(0, tk.END)
-        param_entries[i].insert(0, params[i].strip())
         param_entries[i].grid(row=i, column=1)
         param_name_labels[i].grid(row=i, column=0)
+        params.append(param_entries[i].get().strip())
 
-    for j in range(len(params), 100):
-        param_entries[j].grid_remove()
-        param_name_labels[j].grid_remove()
+    for i in range(len(binding_vars), 100):
+        param_entries[i].grid_remove()
+        param_name_labels[i].grid_remove()
+
+    for i, (param, binding_var) in enumerate(zip(params, binding_vars)):
+        param_name_labels[i].config(text=f"{binding_var}:")
+        param_entries[i].delete(0, tk.END)
+        param_entries[i].insert(0, param.strip())
 
     params_label_var.set(f"Parameters ({len(params)}): {', '.join(params)}")
 
@@ -112,6 +94,8 @@ def update_param_entries():
     update_button.grid(row=10, column=0, pady=(10, 0))
     clipboard_button.grid(row=10, column=1, pady=(10, 0))
 
+def update_param_entries():
+    update_params()
 
 root = tk.Tk()
 root.title("Query Binding")
@@ -127,12 +111,19 @@ params_label_var = tk.StringVar()
 params_label_var.set("Parameters (0):")
 params_label = tk.Label(root, textvariable=params_label_var)
 params_label.grid(row=2, column=0, sticky="w", padx=10, pady=(10, 0))
+params_label.grid_remove()
 
 bind_button = tk.Button(root, text="Bind", command=submit)
-bind_button.grid(row=3, column=0, sticky="w", padx=10, pady=(10, 0))
+bind_button.grid(row=3, column=0, sticky="w", padx=5, pady=(10, 0))
+
+update_button = tk.Button(root, text="Update Parameters", command=update_param_entries)
+update_button.grid(row=6, column=0, sticky="w",  padx=5, pady=(10, 0))
+
+clipboard_button = tk.Button(root, text="Copy to Clipboard", command=copy_to_clipboard)
+clipboard_button.grid(row=6, column=0, sticky="e",  padx=5, pady=(10, 0))
 
 clear_button = tk.Button(root, text="Clear", command=clear)
-clear_button.grid(row=3, column=0, sticky="e", padx=10, pady=(10, 0))
+clear_button.grid(row=3, column=0, sticky="e", padx=5, pady=(10, 0))
 
 result_label = tk.Label(root, text="Result:")
 result_label.grid(row=4, column=0, sticky="w", pady=(10, 0))
@@ -143,12 +134,7 @@ params_frame = tk.Frame(root)
 params_frame.grid(row=1, column=1, rowspan=5, sticky="nsew", padx=(10, 0))
 
 param_entries = [tk.Entry(params_frame, width=10) for _ in range(100)]
-param_name_labels = [tk.Label(params_frame, text=f"Var {i + 1}:") for i in range(100)]
-
-update_button = tk.Button(params_frame, text="Update Parameters", command=update_param_entries)
-update_button.grid(row=10, column=0, pady=(10, 0))
-
-clipboard_button = tk.Button(params_frame, text="Copy to Clipboard", command=copy_to_clipboard)
-clipboard_button.grid(row=10, column=1, pady=(10, 0))
+param_name_labels = [tk.Label(params_frame, text=f"Var {i+1}:") for i in range(100)]
 
 root.mainloop()
+
