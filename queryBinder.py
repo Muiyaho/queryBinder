9227ecc257d7f64cb70bd37ed5edb3bdffab1c33
Muiyaho <discordBotMaybe@gmail.com>
2023-04-24 15:28:09 +0900

    1.2 UI 수정 (pyQt5로 수정)

--- queryBinder.py
+++ queryBinder.py
@@ -1,166 +1,185 @@
-import tkinter as tk
-from tkinter import scrolledtext, ttk
+import sys
 import re
-import tkinter.messagebox as messagebox
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QPushButton, QLabel,
+                             QTextEdit, QLineEdit, QScrollArea, QWidget, QFrame, QMessageBox, QSizePolicy)
+from PyQt5.QtCore import Qt
+
+class QueryBindingApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Query Binding")
+        self.setFixedSize(700, 500)
+
+        self.initUI()
+
+    def initUI(self):
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        main_layout = QHBoxLayout()
+        central_widget.setLayout(main_layout)
+
+        left_layout = QVBoxLayout()
+        main_layout.addLayout(left_layout)
+
+        self.query_label = QLabel("Log Text:")
+        left_layout.addWidget(self.query_label)
+
+        self.query_text = QTextEdit()
+        left_layout.addWidget(self.query_text)
+
+        self.params_label = QLabel("Parameters (0):")
+        left_layout.addWidget(self.params_label)
+        self.params_label.hide()
+
+        self.result_label = QLabel("Result:")
+        left_layout.addWidget(self.result_label)
+
+        self.result_text = QTextEdit()
+        left_layout.addWidget(self.result_text)
+
+        button_layout = QHBoxLayout()
+        left_layout.addLayout(button_layout)
+
+        self.bind_button = QPushButton("Bind")
+        button_layout.addWidget(self.bind_button)
+        self.bind_button.clicked.connect(lambda: self.submit())
+
+        self.clear_button = QPushButton("Clear")
+        button_layout.addWidget(self.clear_button)
+        self.clear_button.clicked.connect(self.clear)
+
+        self.update_button = QPushButton("Update Parameters")
+        button_layout.addWidget(self.update_button)
+        self.update_button.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.update_button.clicked.connect(self.update_param_entries)
+        self.update_button.hide()
+
+        self.clipboard_button = QPushButton("Copy to Clipboard")
+        button_layout.addWidget(self.clipboard_button)
+        self.clipboard_button.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.clipboard_button.clicked.connect(self.copy_to_clipboard)
+        self.clipboard_button.hide()
+
+        self.scroll_area = QScrollArea()
+        main_layout.addWidget(self.scroll_area)
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+
+        self.entries_frame = QFrame()
+        self.scroll_area.setWidget(self.entries_frame)
+
+        self.entries_layout = QVBoxLayout()
+        self.entries_frame.setLayout(self.entries_layout)
+
+        self.param_entries = [QLineEdit() for _ in range(100)]
+        self.param_name_labels = [QLabel(f"Var {i + 1}:") for i in range(100)]
+
+        for i in range(100):
+            entry_label_layout = QHBoxLayout()
+            self.entries_layout.addLayout(entry_label_layout)
+
+            entry_label_layout.addWidget(self.param_name_labels[i])
+            entry_label_layout.addWidget(self.param_entries[i])
+
+            self.param_entries[i].hide()
+            self.param_name_labels[i].hide()
+
+            self.entries_layout.addStretch(1)  # 이 줄을 추가하여 파라미터 영역이 더 넓어지도록 합니다.
+
+        main_layout.setStretch(0, 7)  # 왼쪽 레이아웃의 비율을 줄입니다.
+        main_layout.setStretch(1, 3)  # 오른쪽 레이아웃의 비율을 늘립니다.
+    def bind_query(self, query_template, params):
+        query_template = re.sub(r'\?', r"'{}'", query_template)
+        return query_template.format(*params)
+
+    def extract_query_params(self, log_text):
+        param_pattern = r'Parameters: \[(.*?)\]'
+        param_match = re.search(param_pattern, log_text)
+
+        params = param_match.group(1).split(',') if param_match else []
+        return params
+
+    def extract_binding_vars(self, query_template):
+        matches = re.findall(r"(\w+)\s*=\s*\?", query_template)
+        question_marks = query_template.count("?")
+
+        while len(matches) < question_marks:
+            matches.append("temp")
+
+        return matches
+
+    def submit(self):
+        log_text = self.query_text.toPlainText().strip()
+        query_template = log_text.split("DEBUG")[0].strip()
+
+        params = self.extract_query_params(log_text)
+        binding_vars = self.extract_binding_vars(query_template)
+
+        for i, (param, binding_var) in enumerate(zip(params, binding_vars)):
+            self.param_name_labels[i].setText(f"{binding_var}:")
+            self.param_entries[i].setText(param.strip())
+
+            self.params_label.setText(f"Parameters ({len(params)}): {', '.join(params)}")
+            self.params_label.hide()
+
+            result = self.bind_query(query_template, params)
+            self.result_text.clear()
+            self.result_text.insertPlainText(result)
+
+            self.update_button.show()
+            self.clipboard_button.show()
+            self.update_params()
 
 
-def bind_query(query_template, params):
-    query_template = re.sub(r'\?', r"'{}'", query_template)
-    return query_template.format(*params)
+    def clear(self):
+        self.query_text.clear()
+        self.result_text.clear()
+        self.params_label.setText("Parameters (0):")
+        self.params_label.hide()
+        for entry, var_name_label in zip(self.param_entries, self.param_name_labels):
+            entry.hide()
+            var_name_label.hide()
 
+    def copy_to_clipboard(self):
+        clipboard = QApplication.clipboard()
+        clipboard.clear()
+        clipboard.setText(self.result_text.toPlainText().strip())
+        QMessageBox.information(self, "복사 완료", "클립보드로 복사되었습니다.")
 
-def extract_query_params(log_text):
-    param_pattern = r'Parameters: \[(.*?)\]'
-    param_match = re.search(param_pattern, log_text)
+    def update_params(self):
+        log_text = self.query_text.toPlainText().strip()
+        query_template = log_text.split("DEBUG")[0].strip()
+        binding_vars = self.extract_binding_vars(log_text)
 
-    params = param_match.group(1).split(',') if param_match else []
-    return params
+        params = []
+        for i, binding_var in enumerate(binding_vars):
+            self.param_name_labels[i].setText(f"{binding_var}:")
+            self.param_entries[i].show()
+            self.param_name_labels[i].show()
+            params.append(self.param_entries[i].text().strip())
 
+        for i in range(len(binding_vars), 100):
+            self.param_entries[i].hide()
+            self.param_name_labels[i].hide()
 
-def extract_binding_vars(query_template):
-    matches = re.findall(r"(\w+)\s*=\s*\?", query_template)
-    question_marks = query_template.count("?")
+        for i, (param, binding_var) in enumerate(zip(params, binding_vars)):
+            self.param_name_labels[i].setText(f"{binding_var}:")
+            self.param_entries[i].setText(param.strip())
 
-    while len(matches) < question_marks:
-        matches.append("temp")
+        self.params_label.setText(f"Parameters ({len(params)}): {', '.join(params)}")
 
-    return matches
+        result = self.bind_query(query_template, params)
+        self.result_text.clear()
+        self.result_text.insertPlainText(result)
 
+    def update_param_entries(self):
+        self.update_params()
 
-def submit():
-    log_text = query_text.get("1.0", tk.END).strip()
-    query_template = log_text.split("DEBUG")[0].strip()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    query_binding_app = QueryBindingApp()
+    query_binding_app.show()
+    sys.exit(app.exec_())
 
-    params = extract_query_params(log_text)
-    binding_vars = extract_binding_vars(query_template)
-
-    for i, (param, binding_var) in enumerate(zip(params, binding_vars)):
-        param_name_labels[i].config(text=f"{binding_var}:")
-        param_entries[i].delete(0, tk.END)
-        param_entries[i].insert(0, param.strip())
-
-    params_label_var.set(f"Parameters ({len(params)}): {', '.join(params)}")
-
-    result = bind_query(query_template, params)
-    result_text.delete("1.0", tk.END)
-    result_text.insert(tk.END, result)
-
-    update_button.grid(row=10, column=0, pady=(10, 0))
-    clipboard_button.grid(row=10, column=1, pady=(10, 0))
-    update_params()
-
-def clear():
-    query_text.delete("1.0", tk.END)
-    result_text.delete("1.0", tk.END)
-    params_label_var.set("Parameters (0):")
-    for entry, var_name_label in zip(param_entries, param_name_labels):
-        entry.grid_forget()
-        var_name_label.grid_forget()
-    root.geometry("620x433")
-
-def copy_to_clipboard():
-    root.clipboard_clear()
-    root.clipboard_append(result_text.get("1.0", tk.END).strip())
-    messagebox.showinfo("복사 완료", "클립보드로 복사 되었습니다.")
-
-
-def update_params():
-    log_text = query_text.get("1.0", tk.END).strip()
-    query_template = log_text.split("DEBUG")[0].strip()
-    binding_vars = extract_binding_vars(log_text)
-
-    params = []
-    for i, binding_var in enumerate(binding_vars):
-        param_name_labels[i].config(text=f"{binding_var}:")
-        param_entries[i].grid(row=i, column=1)
-        param_name_labels[i].grid(row=i, column=0)
-        params.append(param_entries[i].get().strip())
-
-    for i in range(len(binding_vars), 100):
-        param_entries[i].grid_remove()
-        param_name_labels[i].grid_remove()
-
-    for i, (param, binding_var) in enumerate(zip(params, binding_vars)):
-        param_name_labels[i].config(text=f"{binding_var}:")
-        param_entries[i].delete(0, tk.END)
-        param_entries[i].insert(0, param.strip())
-
-    params_label_var.set(f"Parameters ({len(params)}): {', '.join(params)}")
-
-    result = bind_query(query_template, params)
-    result_text.delete("1.0", tk.END)
-    result_text.insert(tk.END, result)
-
-    update_button.grid(row=10, column=0, pady=(10, 0))
-    root.geometry("620x435")
-
-def update_param_entries():
-    update_params()
-
-root = tk.Tk()
-root.title("Query Binding")
-
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
-
-frame = tk.Frame(root)
-frame.grid(row=0, column=0, sticky="nsew")
-
-# Configure rows and columns for dynamic resizing
-frame.columnconfigure(0, weight=1)
-frame.rowconfigure(0, weight=1)
-frame.rowconfigure(1, weight=1)
-frame.rowconfigure(2, weight=1)
-frame.rowconfigure(3, weight=1)
-
-query_label = tk.Label(frame, text="Log Text:")
-query_label.grid(row=0, column=0, sticky="nw", pady=(10, 0))
-query_text = scrolledtext.ScrolledText(frame, wrap=tk.WORD, width=50, height=10)
-query_text.grid(row=1, column=0, sticky="nsew", padx=10)
-
-params_label_var = tk.StringVar()
-params_label_var.set("Parameters (0):")
-params_label = tk.Label(frame, textvariable=params_label_var)
-params_label.grid(row=2, column=0, sticky="w", padx=10, pady=(10, 0))
-params_label.grid_remove()
-
-bind_button = tk.Button(frame, text="Bind", command=submit)
-bind_button.grid(row=3, column=0, sticky="w", padx=5, pady=(10, 0))
-
-update_button = tk.Button(frame, text="Update Parameters", command=update_param_entries)
-update_button.grid(row=6, column=0, sticky="w",  padx=5, pady=(10, 0))
-
-clipboard_button = tk.Button(frame, text="Copy to Clipboard", command=copy_to_clipboard)
-clipboard_button.grid(row=6, column=0, sticky="e",  padx=5, pady=(10, 0))
-
-clear_button = tk.Button(frame, text="Clear", command=clear)
-clear_button.grid(row=3, column=0, sticky="e", padx=5, pady=(10, 0))
-
-result_label = tk.Label(frame, text="Result:")
-result_label.grid(row=4, column=0, sticky="w", pady=(10, 0))
-result_text = scrolledtext.ScrolledText(frame, wrap=tk.WORD, width=50, height=10)
-result_text.grid(row=5, column=0, sticky="nsew", padx=10)
-
-params_frame = tk.Frame(frame)
-params_frame.grid(row=1, column=1, rowspan=5, sticky="nsew", padx=(10, 0))
-
-# Adding a canvas for scrolling functionality
-params_canvas = tk.Canvas(params_frame, width=200)
-params_canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-
-# Adding a scrollbar
-scrollbar = ttk.Scrollbar(params_frame, orient="vertical", command=params_canvas.yview)
-scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-
-# Configuring the canvas
-params_canvas.configure(yscrollcommand=scrollbar.set)
-params_canvas.bind('<Configure>', lambda e: params_canvas.configure(scrollregion=params_canvas.bbox("all")))
-
-# Adding a frame for the param_entries and param_name_labels
-entries_frame = tk.Frame(params_canvas)
-params_canvas.create_window((0, 0), window=entries_frame, anchor="nw")
-
-param_entries = [tk.Entry(entries_frame, width=10) for _ in range(100)]
-param_name_labels = [tk.Label(entries_frame, text=f"Var {i+1}:") for i in range(100)]
-
-root.mainloop()
